81ff076b61d3feeb0f96459319e6bc4a7bf6c08d
dariaoana <dariabd255@gmail.com>
2026-03-26 13:23:07 +0200

    miau

--- src/main/java/com/galati/sesizari/controller/SesizariController.java
+++ src/main/java/com/galati/sesizari/controller/SesizariController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 
 @Controller
+@RequestMapping("/sesizari-galati")
 public class SesizariController {
 
     @Autowired

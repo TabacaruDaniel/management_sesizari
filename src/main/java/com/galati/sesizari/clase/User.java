a662a026e3eaa04ba7e2b177fbd206d18e053ada
danie <danieltabacaru5@gmail.com>
2026-03-26 09:23:12 +0200

    update

--- src/main/java/com/galati/sesizari/clase/User.java
+++ src/main/java/com/galati/sesizari/clase/User.java
@@ -18,5 +18,11 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Rol rol;
 
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
 
 }
